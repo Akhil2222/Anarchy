6f16685853097bab35491487caf898f4d2c7fcc3
Akhil2222 <akhily@Akhils-MacBook-Pro.local>
2022-08-28 15:50:10 -0500

    Debug image display

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ fetch('troops.json').then(response => response.json()).then(troops => {
     console.log(troops)
     var img = showtroops(troops)
     for(var i of img){
-        document.body.innerHTML += `<img src='Images/Troops/troops_${i}.svg'/>`
+        document.body.innerHTML += `<img src='Images/Troops/troop_${i}.svg' height='100px'/>`
     }
 })
