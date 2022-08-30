ae0fa0ed2e1fb20b3f088bfbece829fd60989c5f
Akhil2222 <70588733+Akhil2222@users.noreply.github.com>
2022-08-30 11:13:31 -0500

    Add seed

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 fetch('troops.json').then(response => response.json()).then(troops => {
     console.log(troops)
 })
+function createSeed(){
+    var str='abcdefghijklmnopqrstuvwxyz'
+    var code = str[4 + Math.ceil(Math.random()*(str.length-4))]
+    for(var i = 0;i < 7;i++){
+        code+=str[Math.round(Math.random()*str.length)]
+    }
+    return code
+}
