0d6bb2e87a61dd62d9f4a67b124262ab505c61a4
Akhil2222 <akhily@akhils-mbp.lan>
2022-08-31 08:19:43 -0500

    Make pseudorandom codes

--- server.js
+++ server.js
@@ -4,12 +4,27 @@ fetch('troops.json').then(response => response.json()).then(troops => {
     for(var i of img){
         document.body.innerHTML += `<img src='Images/Troops/troop_${i}.svg' height='100px'/>`
     }
+    let code = createSeed()
+    console.log(code)
+    console.log(parseInt(code,36).toString(16),parseInt(code,36).toString(16).length)
+    console.log(genval(code,10))
 })
 function createSeed(){
-    var str='abcdefghijklmnopqrstuvwxyz'
-    var code = str[4 + Math.ceil(Math.random()*(str.length-4))]
-    for(var i = 0;i < 7;i++){
-        code+=str[Math.round(Math.random()*str.length)]
-    }
+    let code = Math.floor((16**9)+Math.random()*(16**10 - 16**9)).toString(36)
     return code
+}
+function genval(code,len){
+    var recent = code
+    console.log(recent)
+    var codes = []
+
+    for(var i = 0;i < len;i++){
+        console.log(parseInt(recent,36).toString(16).length)
+        recent = ((parseInt(recent,36)+(parseInt(recent.slice(3),36)**2))%16**10).toString(36)
+        codes.push(recent)
+    }
+    return codes
+}
+function test_resource(codearr,posarr){
+
 }
